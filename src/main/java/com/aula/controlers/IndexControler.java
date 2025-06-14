bec9a674a2f6be4bda3f2c8d1b659166f4b0fdf4
Aluno ProWay <aluno@proway.treina>
2025-06-14 12:23:14 -0300

    excluindo dados do H2

--- src/main/java/com/aula/controlers/IndexControler.java
+++ src/main/java/com/aula/controlers/IndexControler.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.aula.dao.ContatoDao;
@@ -16,6 +17,12 @@ public class IndexControler {
 	@Autowired
 	private ContatoDao cdao;
 
+	@GetMapping("/excluir/{id}")
+	public String delete(@PathVariable Long id) {
+		cdao.delete(id);
+		return "redirect:/contato";
+	}
+	
 	@GetMapping("/novoContato")
 	public String novoContato(Model model) {
 		model.addAttribute("contato", new Contato());
@@ -39,6 +46,8 @@ public class IndexControler {
 		model.addAttribute("lista", cdao.getAll());
 		return "contato";
 	}
+	
+	
 	
 	
 }
--- src/main/java/com/aula/dao/ContatoDao.java
+++ src/main/java/com/aula/dao/ContatoDao.java
@@ -10,19 +10,22 @@ import com.aula.repositories.ContatoRepository;
 
 @Service
 public class ContatoDao {
-  // private static List<Contato> lista = new ArrayList<Contato>();
-   
-   @Autowired
-   private  ContatoRepository repository;
-    
-   public  void add(Contato contato) {
-	 //  lista.add(contato);
-	   repository.save(contato);
-   }
-   
-   public  List<Contato> getAll(){
-	   return repository.findAll();
-}
-   
-   
+	// private static List<Contato> lista = new ArrayList<Contato>();
+
+	@Autowired
+	private ContatoRepository repository;
+
+	public void add(Contato contato) {
+		// lista.add(contato);
+		repository.save(contato);
+	}
+
+	public List<Contato> getAll() {
+		return repository.findAll();
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
 }
